2b1b20c83c2e8dc1f4ca9285221e7d31291e8909
root <remy.vanherweghem@parl.gc.ca>
2022-02-10 13:02:43 +0000

    Removed commented out code.

--- vite.config.js
+++ vite.config.js
@@ -12,22 +12,5 @@ export default defineConfig({
     rollupOptions: {
       input: ['./src/main.js'],
     },
-    /*lib: {
-      entry: path.resolve(__dirname, 'src/main.js'),
-      name: 'MyLib',
-      //fileName: (format) => `my-lib.${format}.js`
-    },*/
-   /*rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue'
-        }
-      }
-    }*/
   }
 })
