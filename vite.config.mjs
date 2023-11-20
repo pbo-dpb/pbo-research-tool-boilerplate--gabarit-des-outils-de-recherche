efb8a1813768b4de039e37d266e68d0b2715eeb0
Rémy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2023-11-20 15:57:02 +0000

    Upgraded to Vite 5.

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const path = require('path')
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"./",
-  plugins: [vue({ compilerOptions: {
-    customElement: true,
-  },})],
+  base: "./",
+  plugins: [vue({
+    compilerOptions: {
+      customElement: true,
+    },
+  })],
   build: {
     manifest: true,
     rollupOptions: {
