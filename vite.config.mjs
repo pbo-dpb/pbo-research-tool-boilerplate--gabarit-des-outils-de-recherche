e3e82bc1cf821d0c4e95c717958c87aef4373160
Remy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2025-10-31 12:30:08 +0000

    Tailwind uses Vite plugin instead of PostCSS.

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [vue({
-    compilerOptions: {
-      customElement: true,
-    },
-  })],
+  plugins: [
+    vue({
+      compilerOptions: {
+        customElement: true,
+      },
+    }),
+    tailwindcss()
+  ],
   build: {
     manifest: true,
     rollupOptions: {
